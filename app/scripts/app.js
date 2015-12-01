29e9f93b3c50132b2110cdc92e9f74568137644a
Muhammad Shahbaz <muhammad.shahbaz@putitout.co.uk>
2015-12-01 15:39:16 +0500

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+var todoApp = angular.module('mytodoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable'
+]);
+
+todoApp.config(function ($routeProvider) {
+    $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+});
+
+
+todoApp.config(function ($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    $httpProvider.defaults.transformRequest = function (data) {
+        if (data === undefined) {
+            return data;
+        }
+        return $.param(data);
+
+    };
+});
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+        .controller('MainCtrl', function ($scope, $http, $q) {
+            $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+            $scope.addTodo = function () {
+                $scope.todos.push($scope.todo);
+                $scope.todo = '';
+            };
+            $scope.removeTodo = function (index) {
+                $scope.todos.splice(index, 1);
+            };
+
+            //Endpoint testing
+            var obj = {};
+            fetch(obj);
+            function fetch(postData) {
+
+                var method = 'POST';
+                var url = "http://kidlr.lotiv.com/api/user/register";
+                var deferred = $q.defer();
+                // var headers = {'auth-token': '{sPjZze9s@4hyBAieLdWJFz2juAdgnnRhsTVC>Wih))J9WT(kr','Content-Type': undefined};
+                $http({method: method, data: {email : 's@s.com', password : ''}, url: url, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).
+                        then(function (data) {
+                            deferred.resolve(data);
+                        }, function (error) {
+                            deferred.reject(error);
+                        });
+                return deferred.promise;
+
+            }
+            ;
+        });
+        
